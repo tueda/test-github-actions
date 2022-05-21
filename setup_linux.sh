c01960237e8cdf5a488fee3003e783957b4f50ef
Takahiro Ueda <tueda@st.seikei.ac.jp>
2022-05-21 18:56:17 +0900

    ci: test

--- setup_linux.sh
+++ setup_linux.sh
@@ -4,13 +4,46 @@
 
 OS_VERSION=$(lsb_release -rs)
 
-echo RUNNER_ARCH="$RUNNER_ARCH"
-echo RUNNER_NAME="$RUNNER_NAME"
-echo RUNNER_OS="$RUNNER_OS"
-echo RUNNER_TEMP="$RUNNER_TEMP"
-echo RUNNER_TOOL_CACHE="$RUNNER_TOOL_CACHE"
-echo OS_VERSION="$OS_VERSION"
-echo INPUT_FORTRAN_COMPILER="$INPUT_FORTRAN_COMPILER"
+if [[ $INPUT_FORTRAN_COMPILER == default ]]; then
+  case $OS_VERSION in
+    18.04)
+      INPUT_FORTRAN_COMPILER=gfortran-7
+      ;;
+    20.04)
+      INPUT_FORTRAN_COMPILER=gfortran-9
+      ;;
+    22.04)
+      INPUT_FORTRAN_COMPILER=gfortran-11
+      ;;
+  esac
+fi
+
+if [[ $INPUT_FORTRAN_COMPILER == latest ]]; then
+  case $OS_VERSION in
+    18.04)
+      INPUT_FORTRAN_COMPILER=gfortran-11
+      ;;
+  esac
+fi
+
+case $OS_VERSION:$INPUT_FORTRAN_COMPILER in
+  18.04:gfortran-7|\
+  18.04:gfortran-9|\
+  18.04:gfortran-10|\
+  20.04:gfortran-9|\
+  20.04:gfortran-10|\
+  22.04:gfortran-9|\
+  22.04:gfortran-10|\
+  22.04:gfortran-11)
+    # already installed
+    ;;
+  *:gfortran-4.[1-9]|\
+  *:gfortran-[5-9]|\
+  *:gfortran-[1-9][0-9])
+    sudo apt-get update -qq
+    sudo apt-get install -y -qq "$INPUT_FORTRAN_COMPILER"
+    ;;
+esac
 
 echo "FC=gfortran" >>"$GITHUB_ENV"
 echo "FPM_FC=gfortran" >>"$GITHUB_ENV"
